7f2dac1b4e54e15546f03a9ad731a969e3612c03
halisa <halisasaipulaa@gmail.com>
2023-01-29 13:17:17 -0800

    Fetched --- Shipping Options

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -4,6 +4,7 @@ import { useForm, FormProvider } from 'react-hook-form';
 import { commerce } from '../../lib/commerce';
 import FormInput from './FormInput';
 
+
 const AddressForm = ({checkoutToken}) => {
     const [shippingCountries, setShippingCountries] = useState([]);
     const [shippingCountry, setShippingCountry] = useState('');
@@ -14,7 +15,7 @@ const AddressForm = ({checkoutToken}) => {
     const methods = useForm();
     const countries = Object.entries(shippingCountries).map(([code, name]) =>({ id:code, label:name}));
     const subdivisions = Object.entries(shippingSubdivisions).map(([code, name]) =>({ id:code, label:name}));
-
+    const options = shippingOptions.map((sO) =>({id:sO.id, label:`${sO.description}-(${sO.price.formatted_with_symbol})`}) )
 
 
     const fetchShippingCountries = async (checkoutTokenId) => {
@@ -31,25 +32,37 @@ const AddressForm = ({checkoutToken}) => {
         setShippingSubdivision(Object.keys(subdivisions)[0]);
     }
 
+    const fetchShippingOptions = async (checkoutTokenId, country, region=null) =>{
+        const options = await commerce.checkout.getShippingOptions(checkoutTokenId, { country, region})
+    
+        setShippingOptions(options);
+        setShippingOption(options[0].id);
+    }
+
     useEffect(() => {
         fetchShippingCountries(checkoutToken.id)
     },[])
 
     useEffect(() => {
         if(shippingCountry) fetchSubdivisions(shippingCountry)
-    }, [shippingCountry])
+    }, [shippingCountry]);
+
+    useEffect(() => {
+        if(shippingSubdivision) fetchShippingOptions(checkoutToken.id, shippingCountry, shippingSubdivision)
+    }, [shippingSubdivision])
+
     return (
         <>
             <Typography variant='h6' gutterBottom>Shipping Address</Typography>
             <FormProvider {...methods}>
                 <form onSubmit="">
                     <Grid container spacing={3}>
-                        <FormInput required name="firstName" label="First Name" />
-                        <FormInput required name="lastName" label="Last Name" />
-                        <FormInput required name="address1" label="Address" />
-                        <FormInput required name="email" label="Email" />
-                        <FormInput required name="city" label="City" />
-                        <FormInput required name="zip" label="ZIP / Postal Code" />
+                        <FormInput name="firstName"label="First Name" />
+                        <FormInput name="lastName" label="Last Name" />
+                        <FormInput name="address1" label="Address" />
+                        <FormInput name="email" label="Email" />
+                        <FormInput name="city" label="City" />
+                        <FormInput name="zip" label="ZIP / Postal Code" />
                         <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Country</InputLabel>
                             <Select value={shippingCountry} fullwidth onChange={(e)=>setShippingCountry(e.target.value)}>
@@ -70,13 +83,18 @@ const AddressForm = ({checkoutToken}) => {
                                 ))}
                             </Select>
                         </Grid>
-                        {/*<Grid item xs={12} sm={6}>
+                        <Grid item xs={12} sm={6}>
                             <InputLabel>Shipping Options</InputLabel>
-                            <Select value={} fullwidth onChange={}>
-                                <MenuItem key={} value={}>Select Me</MenuItem>
+                            <Select value={shippingOption} fullwidth onChange={(e)=>setShippingOption(e.target.value)}>
+                                {options.map((option) =>(
+                                    <MenuItem key={option.id} value={option.id}>
+                                        {option.label}
+                                    </MenuItem> 
+                                ))}
                             </Select>
-                        </Grid> */}
+                        </Grid> 
                     </Grid>
+                    
 
                 </form>
             </FormProvider>
