66a7e16f99614b3661503286c45cc09f363dfaf8
lisa1501 <halisasaipulla@gmail.com>
2023-01-29 13:32:39 -0800

    implemented Back to cart and Next step feature

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -3,9 +3,9 @@ import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@materia
 import { useForm, FormProvider } from 'react-hook-form';
 import { commerce } from '../../lib/commerce';
 import FormInput from './FormInput';
+import { Link } from 'react-router-dom';
 
-
-const AddressForm = ({checkoutToken}) => {
+const AddressForm = ({ checkoutToken, next }) => {
     const [shippingCountries, setShippingCountries] = useState([]);
     const [shippingCountry, setShippingCountry] = useState('');
     const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
@@ -55,7 +55,7 @@ const AddressForm = ({checkoutToken}) => {
         <>
             <Typography variant='h6' gutterBottom>Shipping Address</Typography>
             <FormProvider {...methods}>
-                <form onSubmit="">
+                <form onSubmit={methods.handleSubmit((data)=>next({ ...data, shippingCountry, shippingSubdivision, shippingOption }))}>
                     <Grid container spacing={3}>
                         <FormInput name="firstName"label="First Name" />
                         <FormInput name="lastName" label="Last Name" />
@@ -94,8 +94,12 @@ const AddressForm = ({checkoutToken}) => {
                             </Select>
                         </Grid> 
                     </Grid>
-                    
-
+                    <br />
+                    <div style={{ display:'flex', justifyContent:'space-between'}}>
+                                <Button component={Link} to="/cart" variant='outlined'>Back to cart</Button>
+                                <Button type="submit"varian='contained'>Next</Button>
+                    </div>
+                
                 </form>
             </FormProvider>
         </>
