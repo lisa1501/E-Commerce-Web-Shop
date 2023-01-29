2fd4054156be14d95aa1ef5b3590edcef63a891c
halisa <halisasaipulaa@gmail.com>
2023-01-29 15:12:01 -0800

    Payment Form & Stripe

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 const App = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState({});
+    const [order, setOrder] = useState({})
+    const [errorMessage, setErrorMessage] = useState('');
 
     const fetchProducts = async () =>{
         const { data } = await commerce.products.list();
@@ -42,6 +44,23 @@ const App = () => {
     
         setCart(item);
     }
+
+    const refreshCart = async() =>{
+        const newCart = await commerce.cart.refresh();
+        setCart(newCart)
+
+    }
+
+    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+        try {
+            const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder)
+            
+            setOrder(incomingOrder);
+            refreshCart();
+        }catch(error) {
+            setErrorMessage(error.data.error.message)
+        }
+    }
     
     useEffect(() => {
         fetchProducts();
@@ -64,6 +83,9 @@ const App = () => {
                     />} />
                     <Route path="/checkout" element={<Checkout 
                     cart={cart}
+                    order={order}
+                    onCaptureCheckout={handleCaptureCheckout}
+                    error={errorMessage}
                     />}/>
                 </Routes>
             </div>
